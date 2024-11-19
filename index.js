a8cd9286ae5162dadd5db65a410f198bbbea934e
Ubuntu <ubuntu@ip-172-31-82-92.ec2.internal>
2024-11-19 15:09:02 +0000

    fix cors policy

--- index.js
+++ index.js
@@ -51,7 +51,6 @@ var corsOptions = {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     Credentials: true
 }
-app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
